30a07ad91da2c2cbb6d250fe6d19830ce8c2a7a7
Rizzo Domenico <domy1riz@gmail.com>
2024-03-08 20:34:52 +0100

    Created a class "DataBaseManager" that will upload logs to mongodbDB

---
+++ DBmanager/src/main/java/DBmanager/DataBaseManager.java
@@ -0,0 +1,39 @@
+package DBmanager;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+public class DataBaseManager {
+    public static void main(String[] args) {
+
+        String uri = "mongodb://localhost";
+        // Construct a ServerApi instance using the ServerApi.builder() method
+        ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
+        MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(new ConnectionString(uri))
+                .serverApi(serverApi).build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            MongoDatabase database = mongoClient.getDatabase("RailwayDB");
+            try {
+                // Send a ping to confirm a successful connection
+                Bson command = new BsonDocument("ping", new BsonInt64(1));
+                Document commandResult = database.runCommand(command);
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+
+                MongoCollection<Document> collection = database.getCollection("RailwayDB");
+                Document doc = new Document("train","RE5");
+                collection.insertOne(doc);
+                System.out.println("Insert compelted");
+            } catch (MongoException me) {
+                System.err.println(me);
+            }
+        }
+    }
+}
--- FogNodesDR/src/main/java/fognodes/FogNodeMain.java
+++ FogNodesDR/src/main/java/fognodes/FogNodeMain.java
@@ -1,11 +1,13 @@
 package fognodes;
 
+import DBmanager.DataBaseManager;
+
 public class FogNodeMain {
     //Train route definition
     public static String[] fogNodes = {"node_A", "node_B", "node_C", "node_D", "node_E"};
     public static void main(String[] args) {
         String broker = "tcp://localhost:1883"; //Mosquitto broker address (local)
-
+        DataBaseManager.main(args);
         //for each node is created a MQTT client as a thread
         for (String nodeTopic : fogNodes) {
             String clientId = "FogNodeSubscriber_" + nodeTopic.substring(nodeTopic.lastIndexOf('/') + 1);
