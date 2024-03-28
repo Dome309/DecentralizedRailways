cefc5fb452901323f7e60db6ec0052d087bed3c5
Rizzo Domenico <domy1riz@gmail.com>
2024-03-28 12:45:11 +0100

    Different colors are showed based on train route lines

--- TrainDR/src/main/java/train/UI/TrainWaypointRender.java
+++ TrainDR/src/main/java/train/UI/TrainWaypointRender.java
@@ -9,19 +9,20 @@ import java.awt.geom.Point2D;
 
 public class TrainWaypointRender implements WaypointRenderer<Waypoint> {
     private static final int CIRCLE_RADIUS = 10;
-    private static final Color COLOR = Color.BLUE;
 
     @Override
     public void paintWaypoint(Graphics2D graphics, JXMapViewer map, Waypoint waypoint) {
         Point2D point = map.getTileFactory().geoToPixel(waypoint.getPosition(), map.getZoom());
-        graphics.setColor(COLOR);
-        int circleX = (int) point.getX() - CIRCLE_RADIUS;
-        int circleY = (int) point.getY() - CIRCLE_RADIUS;
-        graphics.fillOval(circleX, circleY, CIRCLE_RADIUS * 2, CIRCLE_RADIUS * 2);
 
         if (waypoint instanceof TrainCustomWaypoint) {
             TrainCustomWaypoint customWaypoint = (TrainCustomWaypoint) waypoint;
             String label = customWaypoint.getLabel();
+            Color color = getColorForLabel(label);
+            graphics.setColor(color);
+            int circleX = (int) point.getX() - CIRCLE_RADIUS;
+            int circleY = (int) point.getY() - CIRCLE_RADIUS;
+            graphics.fillOval(circleX, circleY, CIRCLE_RADIUS * 2, CIRCLE_RADIUS * 2);
+
             graphics.setColor(Color.WHITE);
             FontMetrics fm = graphics.getFontMetrics();
             int labelWidth = fm.stringWidth(label);
@@ -29,6 +30,18 @@ public class TrainWaypointRender implements WaypointRenderer<Waypoint> {
             int startX = circleX + CIRCLE_RADIUS - labelWidth / 2;
             int startY = circleY + CIRCLE_RADIUS + labelHeight / 4;
             graphics.drawString(label, startX, startY);
+        }
+    }
+
+    private Color getColorForLabel(String label) {
+        if (label.startsWith("RE")) {
+            return Color.RED;
+        } else if (label.startsWith("X")) {
+            return Color.ORANGE;
+        } else if (label.startsWith("R")) {
+            return Color.BLUE;
+        } else {
+            return Color.GREEN;
         }
     }
 }
