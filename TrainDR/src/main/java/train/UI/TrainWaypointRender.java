3acaa2cace184d490e0658e3da68a587f23bcc1b
Rizzo Domenico <domy1riz@gmail.com>
2024-03-29 15:00:15 +0100

    changed the graphics of train waypoints

--- TrainDR/src/main/java/train/UI/TrainWaypointRender.java
+++ TrainDR/src/main/java/train/UI/TrainWaypointRender.java
@@ -3,32 +3,31 @@ package train.UI;
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.viewer.Waypoint;
 import org.jxmapviewer.viewer.WaypointRenderer;
-
 import java.awt.*;
 import java.awt.geom.Point2D;
 
 public class TrainWaypointRender implements WaypointRenderer<Waypoint> {
-    private static final int CIRCLE_RADIUS = 10;
+    private static final int SIZE = 14;
+    private static final int HALF_SIZE = SIZE / 2;
 
     @Override
     public void paintWaypoint(Graphics2D graphics, JXMapViewer map, Waypoint waypoint) {
         Point2D point = map.getTileFactory().geoToPixel(waypoint.getPosition(), map.getZoom());
+        int[] xPoints = {(int) point.getX(), (int) point.getX() - HALF_SIZE, (int) point.getX() + HALF_SIZE};
+        int[] yPoints = {(int) point.getY(), (int) point.getY() - SIZE, (int) point.getY() - SIZE};
 
         if (waypoint instanceof TrainCustomWaypoint) {
             TrainCustomWaypoint customWaypoint = (TrainCustomWaypoint) waypoint;
             String label = customWaypoint.getLabel();
             Color color = getColorForLabel(label);
             graphics.setColor(color);
-            int circleX = (int) point.getX() - CIRCLE_RADIUS;
-            int circleY = (int) point.getY() - CIRCLE_RADIUS;
-            graphics.fillOval(circleX, circleY, CIRCLE_RADIUS * 2, CIRCLE_RADIUS * 2);
-
-            graphics.setColor(Color.WHITE);
+            graphics.fillPolygon(new Polygon(xPoints, yPoints, 3));
+            graphics.setColor(Color.BLACK);
             FontMetrics fm = graphics.getFontMetrics();
             int labelWidth = fm.stringWidth(label);
             int labelHeight = fm.getHeight();
-            int startX = circleX + CIRCLE_RADIUS - labelWidth / 2;
-            int startY = circleY + CIRCLE_RADIUS + labelHeight / 4;
+            int startX = (int) point.getX() - labelWidth / 2;
+            int startY = (int) point.getY() - SIZE - labelHeight / 4;
             graphics.drawString(label, startX, startY);
         }
     }
