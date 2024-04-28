aa8983f0684113f3d6f0d0695b9809026e37e5f5
Rizzo Domenico <domy1riz@gmail.com>
2024-04-28 16:18:11 +0200

    NodeUI class interface improved

--- FogNodesDR/src/main/java/fognodes/UI/NodeUI.java
+++ FogNodesDR/src/main/java/fognodes/UI/NodeUI.java
@@ -3,27 +3,36 @@ package fognodes.UI;
 import org.jxmapviewer.viewer.GeoPosition;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
 
 public class NodeUI {
     public NodeUI(String label, GeoPosition pos){
         JFrame nodeFrame = new JFrame("Node Information");
-        nodeFrame.setSize(400, 550);
+        nodeFrame.setSize(300, 450);
         nodeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        nodeFrame.setResizable(false);
         nodeFrame.setLocationRelativeTo(null);
-        nodeFrame.setLayout(null);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        ImageIcon imgNode = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/node.png")));
+        JLabel labelIcon = new JLabel(imgNode);
+        labelIcon.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JLabel labelInfo = new JLabel(label);
-        labelInfo.setBounds(0, 0, 200, 20);
+        labelInfo.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JLabel labelCoordinates = new JLabel(String.valueOf(pos));
-        labelCoordinates.setBounds(0, 50, 200, 20);
+        labelCoordinates.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        nodeFrame.add(labelInfo);
-        nodeFrame.add(labelCoordinates);
+        mainPanel.add(Box.createVerticalStrut(20));
+        mainPanel.add(labelIcon);
+        mainPanel.add(labelInfo);
+        mainPanel.add(labelCoordinates);
+
+        nodeFrame.add(mainPanel);
         nodeFrame.setVisible(true);
-    }
-
-    private void addTrainCounter(){
-
     }
 }
