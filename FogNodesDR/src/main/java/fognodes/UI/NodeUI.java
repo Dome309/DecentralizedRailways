5f7a01dfec8d42f4a8985538b299f32c8068f9f7
Rizzo Domenico <domy1riz@gmail.com>
2024-04-29 21:00:05 +0200

    layouts updated

--- FogNodesDR/src/main/java/fognodes/UI/NodeUI.java
+++ FogNodesDR/src/main/java/fognodes/UI/NodeUI.java
@@ -3,6 +3,7 @@ package fognodes.UI;
 import org.jxmapviewer.viewer.GeoPosition;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.util.Objects;
 
@@ -11,28 +12,41 @@ public class NodeUI {
         JFrame nodeFrame = new JFrame("Node Information");
         nodeFrame.setSize(300, 450);
         nodeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        nodeFrame.setLayout(new GridLayout(2, 1));
         nodeFrame.setResizable(false);
         nodeFrame.setLocationRelativeTo(null);
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-
+        
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        infoPanel.setBorder(new EmptyBorder(20, 0, 0, 0));
+        
         ImageIcon imgNode = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/node.png")));
         JLabel labelIcon = new JLabel(imgNode);
         labelIcon.setAlignmentX(Component.CENTER_ALIGNMENT);
-
+        infoPanel.add(labelIcon);
+        
         JLabel labelInfo = new JLabel(label);
         labelInfo.setAlignmentX(Component.CENTER_ALIGNMENT);
+        infoPanel.add(labelInfo);
 
-        JLabel labelCoordinates = new JLabel(String.valueOf(pos));
-        labelCoordinates.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JPanel coordinatePanel = getjPanel(pos);
 
-        mainPanel.add(Box.createVerticalStrut(20));
-        mainPanel.add(labelIcon);
-        mainPanel.add(labelInfo);
-        mainPanel.add(labelCoordinates);
-
-        nodeFrame.add(mainPanel);
+        nodeFrame.add(infoPanel);
+        nodeFrame.add(coordinatePanel);
         nodeFrame.setVisible(true);
+    }
+
+    private JPanel getjPanel(GeoPosition pos) {
+        JPanel coordinatePanel = new JPanel(new GridLayout(1, 2));
+
+        double latitude = pos.getLatitude();
+        double longitude = pos.getLongitude();
+
+        JLabel labelLatitude = new JLabel("Latitude: " + latitude);
+        coordinatePanel.add(labelLatitude);
+
+        JLabel labelLongitude = new JLabel("Longitude: " + longitude);
+        coordinatePanel.add(labelLongitude);
+        return coordinatePanel;
     }
 }
